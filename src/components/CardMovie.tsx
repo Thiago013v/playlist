4c0459724eb84fa71219f18b2cf4eaf4aa6f6335
Thiago Vieira Ferreira <thiagoferreiravieira1706@gmail.com>
2026-09-09 18:21:19 -0300

    Card Movie refatorado + bug corrigido

--- src/components/CardMovie.tsx
+++ src/components/CardMovie.tsx
@@ -1,6 +1,6 @@
 import { RiDeleteBin2Line } from "react-icons/ri";
 import { ModalConfirmation } from "./ModalConfirmation";
-import { usePlaylist } from "../hooks/usePlaylist";
+import { useState } from "react";
 
 interface CardMovieType {
   Poster: string | undefined;
@@ -14,23 +14,23 @@ interface CardMovieType {
 }
 
 export function CardMovie({ Poster, Id, handleRemove, title }: CardMovieType) {
-  const { isShowModalConfirmation, setIsShowModalConfirmation } = usePlaylist();
+  const [isShowModal, setIsShowModal] = useState<boolean | undefined>();
 
   return (
     <>
       <ModalConfirmation
         id={Id}
-        isShowModalConfirmation={isShowModalConfirmation}
+        isShowModalConfirmation={isShowModal}
         handleRemove={handleRemove}
         title={title}
-        setIsShowModalConfirmation={setIsShowModalConfirmation}
+        setIsShowModalConfirmation={setIsShowModal}
       ></ModalConfirmation>
 
       <div className="flex flex-col w-62.5 justify-center">
         <img src={Poster} alt="Poster do Filme" className="rounded-lg" />
         <button
           className="flex flex-row justify-center items-center gap-2 border-amber-400 border-2 bg-amber-100 mt-1 rounded-lg text-amber-600 font-extrabold cursor-pointer hover:bg-amber-200"
-          onClick={() => setIsShowModalConfirmation(true)}
+          onClick={() => setIsShowModal(true)}
         >
           Remover
           <RiDeleteBin2Line />
