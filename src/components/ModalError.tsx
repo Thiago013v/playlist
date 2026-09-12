2670b07935a2d2a4f79bfefd42330d738809da57
Thiago Vieira Ferreira <thiagoferreiravieira1706@gmail.com>
2026-09-12 20:09:04 -0300

    UI/UX melhorada ao usuário tentar fazer uma requisição à API de filmes estando sem conexão com a rede de internet. Adicionado um botão de recarregamento da página que permite o usuário tentar fazer uma nova requisição.

--- src/components/ModalError.tsx
+++ src/components/ModalError.tsx
@@ -1,5 +1,6 @@
 import { motion, AnimatePresence } from "motion/react";
 import { usePlaylistContext } from "../hooks/usePlaylistContext";
+import { TbReload } from "react-icons/tb";
 
 export function ModalError() {
   const { isShowModalError, typeMovieError } = usePlaylistContext();
@@ -15,12 +16,22 @@ export function ModalError() {
     <AnimatePresence initial={false}>
       {isShowModalError ? (
         <motion.div
-          className={`fixed top-0 left-0 bg-red-400 w-full p-4 z-1000`}
+          className={`flex flex-row justify-center items-center gap-4 fixed top-0 left-0 bg-red-400 w-full p-4 z-1000`}
           initial={{ opacity: 0 }}
           animate={{ opacity: 100 }}
           exit={{ opacity: 0 }}
         >
-          <h2 className="text-2xl text-white text-center">{message}</h2>
+          <form className="flex flex-row justify-center items-center gap-2">
+            <h2 className="text-2xl text-white text-center">{message}</h2>
+            {message === "Sem Internet" && (
+              <button
+                type="submit"
+                className="text-3xl text-white rotate-360 font-extrabold cursor-pointer"
+              >
+                <TbReload />
+              </button>
+            )}
+          </form>
         </motion.div>
       ) : null}
     </AnimatePresence>
